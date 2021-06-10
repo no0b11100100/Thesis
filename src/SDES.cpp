788758c9a0d79aea586025a2bc31df388a67e2ac
drago <fedorovskiy000@gmail.com>
2021-06-10 21:32:51 +0300

    add description for SDES

--- src/SDES.cpp
+++ src/SDES.cpp
@@ -32,26 +32,44 @@ std::tuple<QString, QString> SDES::generateKey(const QString& key, const permuta
 
 void SDES::P_blockCompression(QString& key, const permutationType<8>& P_block)
 {
-    //        qDebug() << "key" << key;
     makePermautation<8>(key, P_block);
+}
+
+template<size_t N>
+QString toStr(const permutationType<N>& data)
+{
+    QString res = "";
+
+    for(const auto& v : data)
+        res += QString::number(v) + ",";
+
+    return res.mid(0, res.size()-1);
 }
 
 QString SDES::keyRound1(const QString& leftPart, const QString& rightPart,
                         const permutationType<8>& P_block)
 {
+    m_description.GetContentDetails() += TAB + "Для формування ключа для першого раунду спочатку необхідно зробити зсув лівої частини на 1 біт вліво: " + Utils::cycleShift(leftPart, Utils::ShiftDirection::Left)
+                                      + ", а правою на 1 біт вправо: " + Utils::cycleShift(rightPart, Utils::ShiftDirection::Right) + "\n";
     QString tempKey = Utils::cycleShift(leftPart, Utils::ShiftDirection::Left)+
             Utils::cycleShift(rightPart, Utils::ShiftDirection::Right);
+    m_description.GetContentDetails() += TAB +  "Після цього необхідно обрати 8 біт із 10 за заданою таблицею: " + toStr(P_block) + " та отримуємо ключ для першого раунду: ";
     P_blockCompression(tempKey, P_block);
+    m_description.GetContentDetails() += tempKey + "\n";
     return tempKey;
 }
 
 QString SDES::keyRound2(const QString& leftPart, const QString& rightPart,
                         const permutationType<8>& P_block)
 {
+    m_description.GetContentDetails() += TAB + "Для формування ключа для першого раунду спочатку необхідно зробити зсув лівої частини на 2 біт вліво: " + Utils::cycleShift(leftPart, Utils::ShiftDirection::Left, 2)
+                                      + ", а правою на 2 біт вправо: " + Utils::cycleShift(rightPart, Utils::ShiftDirection::Right, 2) + "\n";
     QString tempKey = Utils::cycleShift(leftPart, Utils::ShiftDirection::Left, 2)+
             Utils::cycleShift(rightPart, Utils::ShiftDirection::Right, 2);
-    //        qDebug() << "tempKey" << tempKey;
+
+    m_description.GetContentDetails() += TAB +  "Після цього необхідно обрати 8 біт із 10 за заданою таблицею: " + toStr(P_block) + " та отримуємо ключ для другого раунду: ";
     P_blockCompression(tempKey, P_block);
+    m_description.GetContentDetails() += tempKey + "\n";
     return tempKey;
 }
 
@@ -105,32 +123,36 @@ QString SDES::Round(const QString& text, const QString& key, const int& round,
 {
 
     assert(round == 0 || round == 1);
+    m_description.GetContentDetails() += DOUBLE_TAB + "Вхідний текст для раунду: " + text + "\n"
+            + DOUBLE_TAB + "Розіб'ємо текст на дві частини: ";
     auto [L, R] = divideIntoTwoParts(text);
+    m_description.GetContentDetails() += L + " " + R;
     QString resPart = R;
-//    qDebug() << "R" << R;
     ExpandText(R, expanded);
-//    qDebug() << "expanded text" << R;
+    m_description.GetContentDetails() += "\n" + DOUBLE_TAB + "Зробимо розширення правої частини до 8 біт за таблицею: " + toStr(expanded) + ": " + R + "\n";
+    m_description.GetContentDetails() += DOUBLE_TAB + "Після цього необхідно зробити XOR між розширеною правою частиною та ключем: " +
+                                    R + " XOR " + key + " = ";
     auto xorRes = XOR(R,key);
-//    qDebug() << "key" << key << "XOR with key" << xorRes;
-
+    m_description.GetContentDetails() += xorRes + "\n";
     auto [s1Part, s2Part] = divideIntoTwoParts(xorRes);
 
-    QString sBoxesResult = sBox1.at(prepareDataForSBox(s1Part)) + sBox2.at(prepareDataForSBox(s2Part));//SBox1(s1Part) + SBox2(s2Part);
-//    qDebug() << "s1Part" << s1Part << "s2Part" << s2Part
-//            << "sBox1.at" <<  sBox1.at(prepareDataForSBox(s1Part)) << "SBox1" << SBox1(s1Part)
-//            << "sBox2.at" << sBox2.at(prepareDataForSBox(s2Part)) << "SBox2" << SBox2(s2Part);
-//    qDebug() << "s box result" << sBoxesResult;
-    SPermutation(sBoxesResult, sbox);
+    m_description.GetContentDetails() += DOUBLE_TAB + "Далі розбиваємо отриманний результат на дві частини: " + s1Part + " " + s2Part + "\n";
 
+    QString sBoxesResult = sBox1.at(prepareDataForSBox(s1Part)) + sBox2.at(prepareDataForSBox(s2Part));
+    m_description.GetContentDetails() += DOUBLE_TAB + "та обираючи значення із SBOX таблиць отримуємо: " +
+            sBox1.at(prepareDataForSBox(s1Part)) + " " + sBox2.at(prepareDataForSBox(s2Part)) + "\n";
+    SPermutation(sBoxesResult, sbox);
+    m_description.GetContentDetails() += DOUBLE_TAB + "об'єднуємо отримані значення і робимо перестановку за таблицею: " + toStr(sbox)+ "\n";
+    m_description.GetContentDetails() += DOUBLE_TAB + "Отримуємо " + sBoxesResult + "\n";
     QString result = ( XOR(sBoxesResult, L) );
-//    qDebug() << "XOR" << result;
+    m_description.GetContentDetails() += DOUBLE_TAB + "У кінці раунду робимо XOR між отриманими даними та лівою частиною: " + sBoxesResult + "\n";
+            m_description.GetContentDetails() += DOUBLE_TAB + "та поєднуємо " + (round == 0 ? "ліву частину із отриманими даними: " + resPart + result
+                                                                                            : "отримані дані із лівою: " + result + resPart);
     return round == 0 ? resPart + result : result + resPart;
 }
 
 ReturnType SDES::encode(const QVector<QStringList>& tables, const TextType& pack)
 {
-//    qDebug() << "SDES" << tables;
-//    SDES::toMap(tables[0]);
     constexpr int P_10Index = 6;
     constexpr int P_8Index = 7;
     constexpr int IPIndex = 2;
@@ -139,30 +161,35 @@ ReturnType SDES::encode(const QVector<QStringList>& tables, const TextType& pack
     constexpr int IP_1Index = 5;
     constexpr int TEXTIndex = 0;
     constexpr int KEYIndex = 1;
-//    qDebug() << pack.at(KEYIndex);
+    m_description.AddHeader(KEY_GENERATION);
+    m_description.GetContentDetails() += TAB + "Зробимо перестановку для ключа: " + pack.at(P_10Index).at(1) + "\n"
+            + TAB + "Після перестановки отримуємо: ";
     auto [a,b] = generateKey(pack.at(KEYIndex).at(1), SDES::toArray<10>(pack.at(P_10Index).at(1)));
-//    qDebug() << a << b;
+    m_description.GetContentDetails() += a+b + " та розбиваємо на 2 чистини: " + a + " " + b + "\n";
     QString round1Key = keyRound1(a,b, SDES::toArray<8>(pack.at(P_8Index).at(1)));
-//    qDebug() << "key1" << round1Key;
+
     QString round2Key = keyRound2(a,b, SDES::toArray<8>(pack.at(P_8Index).at(1)));
-//    qDebug() << "key2" << round2Key;
+    m_description.AddContent();
+    m_description.AddHeader(ENCODING);
+
+    m_description.GetContentDetails() += TAB + "Зробимо перестановку для тексту за таблицею: " + pack.at(IPIndex).at(1) + ". Отримуємо: ";
     QString textPermut = textPermutation(pack.at(TEXTIndex).at(1), SDES::toArray<8>(pack.at(IPIndex).at(1)));
-//    qDebug() << "IP" << textPermut;
-
-
+    m_description.GetContentDetails() += textPermut + "\n";
     QString encode = textPermut;
             auto s1 = toMap(tables.at(0));
             auto s2 = toMap(tables.at(1));
     for(int i{0}; i < ROUNDS; ++i)
     {
-//        qDebug() << "round text" << encode;
+        m_description.GetContentDetails() += TAB + "Раунд " + QString::number(i+1) + "\n";
         encode = Round(encode, i == 0 ? round1Key : round2Key, i, SDES::toArray<8>(pack.at(EXPANDEDIndex).at(1)),
                        SDES::toArray<4>(pack.at(SBOX_PERMUTATIONIndex).at(1)),
                        s1, s2);
-//        qDebug() << "round result" << encode << "\n\n";
+        m_description.GetContentDetails() += "\n" + TAB + "Pезультат раунду " + QString::number(i+1) + ": " + encode + "\n";
     }
     QString result = textPermutation(encode, SDES::toArray<8>(pack.at(IP_1Index).at(1)));
-    qDebug() << "encode text" << result;
+    m_description.GetContentDetails() += TAB + "Для завершення роботи алгоритму необхідно зробити перестановку: " + pack.at(IP_1Index).at(1) + "\n" +
+                                      TAB + "В результаті отримуємo " + result;
+    m_description.AddContent();
     return {result, m_description};
 }
 
