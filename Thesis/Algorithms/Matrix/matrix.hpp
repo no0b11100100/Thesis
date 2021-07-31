794a71922b46d991decdc96c1eff722572e57ede
drago <fedorovskiy000@gmail.com>
2021-07-31 21:04:53 +0300

    add Matrix add missed files

---
+++ Thesis/Algorithms/Matrix/matrix.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <QDebug>
+
+#include <array>
+
+namespace Matrix
+{
+
+template <class T, unsigned Rows, unsigned Columns>
+class Matrix
+{
+    std::array<std::array<T, Rows>, Columns> m_matrix;
+
+public:
+    Matrix() = default;
+
+    Matrix(const std::initializer_list<std::initializer_list<T>>& matrix)
+    {
+        assert (matrix.size() == Rows);
+        auto rowIt = m_matrix.begin();
+        for(const auto& row : matrix)
+        {
+            assert(row.size() == Columns);
+            auto columnIt = rowIt->begin();
+            for(const auto& item : row)
+            {
+                *columnIt = item;
+                ++columnIt;
+            }
+            ++rowIt;
+        }
+    }
+
+    unsigned rows() const { return Rows; }
+    unsigned columns() const { return Columns; }
+
+    auto Column(int index) const
+    {
+        assert(index >= 0 && index < Columns);
+        std::array<T, Rows> result;
+        auto it = result.begin();
+
+        for(const auto row : m_matrix)
+        {
+            *it = row.at(index);
+            ++it;
+        }
+
+        return result;
+    }
+    auto Row(int index) const
+    {
+        assert(index >= 0 && index < Rows);
+        return m_matrix.at(index);
+    }
+
+    void ChangeRow(int index, const std::initializer_list<T>& rowData)
+    {
+        assert(index >= 0 && index < Rows);
+        assert(rowData < Rows);
+
+        m_matrix.at(index) = rowData;
+    }
+    void ChangeColumn(int index, const std::initializer_list<T>& columnData)
+    {
+        assert(index >= 0 && index < Columns);
+        assert(columnData < Columns);
+
+        auto it = m_matrix.at(index).begin();
+        for(const auto item : columnData)
+        {
+            *it = item;
+            ++it;
+        }
+    }
+
+    T at(int row, int column) const
+    {
+        assert(row >= 0 && row < Rows);
+        assert(column >= 0 && column < Columns);
+
+        return m_matrix.at(row).at(column);
+    }
+
+    T& at(int row, int column)
+    {
+        assert(row >= 0 && row < Rows);
+        assert(column >= 0 && column < Columns);
+
+        return m_matrix.at(row).at(column);
+    }
+
+    friend Matrix operator^(const Matrix& lhs, const Matrix& rhs)
+    {
+        assert(lhs.rows() == rhs.rows());
+        assert(lhs.columns() == rhs.columns());
+        Matrix result;
+
+        for(unsigned i {0}; i < lhs.rows(); ++i)
+            for(unsigned j{0}; j < lhs.columns(); ++j)
+                result.at(i,j) = (lhs.at(i,j)^rhs.at(i,j));
+
+        return result;
+    }
+
+};
+
+
+}
---
+++ Thesis/Models/NavigationList/List.hpp
@@ -0,0 +1,165 @@
+#pragma once
+#include <QObject>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+#include "../../common/const.hpp"
+
+namespace List {
+
+class StringList : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int size READ size CONSTANT);
+
+public:
+    StringList(QObject* parent = nullptr)
+        : QObject(parent)
+    {}
+
+    void add(const QString& str) { m_list.push_back(str); }
+    void clear() { m_list.clear(); }
+    int size() const { return m_list.size(); }
+    Q_INVOKABLE QString at(int index) const { return m_list.at(index); }
+
+private:
+    std::vector<QString> m_list;
+};
+
+class NavigationList : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QObject* list READ list NOTIFY listChanged)
+
+signals:
+    void listChanged(const QString&);
+
+public:
+
+    NavigationList(QObject* parent = nullptr)
+        : QObject{parent},
+          m_currentList{new StringList{parent}}
+    {
+        m_list = {
+            {
+                item(CRYPTOGRAPHY_GROUP, {
+                    item(RC, {
+                        item(RC2, {}),
+                        item(RC4, {}),
+                        item(RC5, {}),
+                        item(RC6, {})
+                    }),
+                    item(XOR, {}),
+                    item(PERMUTATION, {}),
+                    item(REPLACEMENT, {}),
+                    item(RSA, {}),
+                    item(AES, {}),
+                    item(VIGENERE, {})
+                })
+            },
+            {
+                item(HASH_GROUP, {}),
+            },
+        };
+
+        setCurrentList(m_list);
+
+        for(auto& i : m_list)
+            createLinks(i);
+
+    }
+
+    QObject* list() { return m_currentList.get(); }
+
+    Q_INVOKABLE void back()
+    {
+        qDebug() << "back" << (m_current == nullptr);
+        if(m_current != nullptr)
+        {
+            m_current = m_current->_prev;
+            if(m_current == nullptr)
+                setCurrentList(m_list);
+            else
+                setCurrentList(m_current->_list);
+
+            emit listChanged(TRANSIENT);
+        }
+    }
+
+    Q_INVOKABLE void forward(const QString& name)
+    {
+        qDebug() << "forward" << name;
+        if(m_current == nullptr)
+        {
+            auto nameIndex = std::find_if(m_list.cbegin(), m_list.cend(), [name](const item& i){ return i._title == name; });
+            if(nameIndex != m_list.cend())
+            {
+                auto index = std::distance(m_list.cbegin(), nameIndex);
+                m_current = &m_list.at(index);
+                setCurrentList(m_current->_list);
+                emit listChanged(TRANSIENT);
+            }
+        }
+        else
+        {
+            auto nameIndex = std::find_if(m_current->_list.cbegin(), m_current->_list.cend(), [name](const item& i){ return i._title == name; });
+            if(nameIndex != m_current->_list.cend())
+            {
+                auto index = std::distance(m_current->_list.cbegin(), nameIndex);
+                if(!m_current->_list.at(index)._list.empty())
+                {
+                    m_current = &m_current->_list.at(index);
+                    setCurrentList(m_current->_list);
+                    emit listChanged(TRANSIENT);
+                } else {
+                    qDebug() << "no forward list";
+                    emit listChanged(name);
+                }
+            } else {
+                qDebug() << "can not find" << name;
+            }
+        }
+    }
+
+private:
+
+    struct item
+    {
+        QString _title;
+        std::vector<item> _list;
+        item* _prev = nullptr;
+
+        item(const QString& title, const std::vector<item>& list)
+            : _title{title},
+              _list{list}
+        {}
+    };
+
+    void createLinks(item& it)
+    {
+        if(it._list.empty()) return;
+        for(auto& el : it._list)
+        {
+            el._prev = &it;
+            createLinks(el);
+        }
+    }
+
+    void setCurrentList(const std::vector<item>& items)
+    {
+        m_currentList->clear();
+        for(const auto& item : items)
+            m_currentList->add(item._title);
+    }
+
+    std::vector<item> m_list;
+    std::unique_ptr<StringList> m_currentList;
+    item* m_current = nullptr;
+};
+
+
+
+}
